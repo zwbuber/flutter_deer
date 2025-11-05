765efc1b81a9265aadc4aeefa063779074e73893
zwb <2992391367@qq.com>
2025-11-05 11:59:52 +0800

    fix: 默认颜色修改

--- lib/common/provider/theme_provider.dart
+++ lib/common/provider/theme_provider.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_deer/common/constant/colors.dart';
 
 class ThemeProvider extends ChangeNotifier {
-  
   ThemeData getTheme({bool isDarkMode = false}) {
     return ThemeData(
       colorScheme: ColorScheme.fromSeed(
         seedColor: isDarkMode ? Colours.dark_app_main : Colours.app_main,
+        secondary: isDarkMode ? Colours.dark_app_main : Colours.app_main,
+        error: isDarkMode ? Colours.dark_red : Colours.red,
       ),
     );
   }
