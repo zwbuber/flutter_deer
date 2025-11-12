2ec1c77e06afda790e8bc1ba3a99cf82081e5c79
zwb <2992391367@qq.com>
2025-11-12 09:46:46 +0800

    fix: 安卓打包配置

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,17 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+// 加载 keystore.properties 文件
+var keystorePropertiesFile = rootProject.file("app/key.properties")
+var keystoreProperties = Properties()
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(keystorePropertiesFile.inputStream())
 }
 
 android {
@@ -19,6 +28,7 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
+    // 默认配置
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.flutter_deer"
@@ -30,11 +40,33 @@ android {
         versionName = flutter.versionName
     }
 
+    // 签名配置
+    signingConfigs {
+      create("release") {
+          keyAlias = keystoreProperties["keyAlias"] as String
+          keyPassword = keystoreProperties["keyPassword"]  as String
+          storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+          storePassword = keystoreProperties["storePassword"]  as String
+      }
+    }
+    
     buildTypes {
-        release {
+        getByName("release") {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            // signingConfig = signingConfigs.getByName("debug")
+
+            signingConfig = signingConfigs.getByName("release")
+
+            // isMinifyEnabled = true // 开启混淆
+
+            // proguardFiles(
+            //    getDefaultProguardFile("proguard-android.txt"), // 混淆规则（系统自带）
+            //     "proguard-rules.pro"
+            // )
+        }
+        debug {
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
